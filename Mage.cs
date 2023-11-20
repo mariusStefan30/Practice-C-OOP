30388e976af79e49b4ba3baf537f5e4593b8f7af
Marius Stefan <54823269+mariusStefan30@users.noreply.github.com>
2023-11-20 20:14:28 +0000

    Create Mage.cs
    Add next rank Mage which inherits Pupil.

---
+++ Mage.cs
@@ -0,0 +1,21 @@
+//Mage.cs
+using System;
+
+namespace MigicalInheritance
+{
+  //define a Mage class that inherits from Pupil
+  class Mage : Pupil
+  {
+    //add Mage constructor with string argument. It should call the base constructor with same argument
+    public Mage(string title) : base(title) 
+    {
+    }
+
+    //give Mage new spell: CastRainStorm()
+    publuc Storm CastRainStorm()
+    {
+      //construct new Storm and return it, This is a weak storm cast by mage
+      return new Storm("rain", false, Title);
+    }
+  }
+}
